6f88b91aa84ac9943901b2d7d97e56ad03e72b7f
Lucas <lguilenia@gmail.com>
2025-09-25 14:02:28 -0300

    add middleware

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 import express from "express";
+import notFound from "./src/middlewares/not-found.js";
+
 const app = express();
 const port = 3000;
 
+app.use((req, res, next) => {
+  // res.json({message: 'Hola middleWare'})
+  console.log(req.method);
+  next();
+});
+
+app.use(notFound)
 
 app.get("/", (req, res) => {
-  res.end("Bienvenidoo");
+  res.json({ message: "Hola middleWare", hola: "Saludo" });
 });
 
 app.listen(port, () => {
